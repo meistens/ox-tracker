2d8c99d7b9a5b694c22b55837dd681fc862d016e
David Mebo <krankd@proton.me>
2025-06-29 10:02:34 +0100

    added config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,9 +1,55 @@
 package config
 
-// Configuration
+import (
+	"os"
+
+	"github.com/dotenv-org/godotenvvault"
+)
+
+// Configurations
 type Config struct {
-	DatabaseURL string
-	TMDBAPIKey  string
-	BotToken    string
-	Port        string
+	DatabaseURL Database
+	APIKeys     API
+	BotTokens   Bot
+	Server      Server
+}
+
+type Database struct {
+	URL string
+}
+
+type API struct {
+	TMDBKey string
+}
+
+type Bot struct {
+	DiscordToken  string
+	TelegramToken string
+}
+
+type Server struct {
+	Port string
+	Host string
+}
+
+// Load Configurations
+func Load() (*Config, error) {
+	godotenvvault.Load()
+
+	return &Config{
+		DatabaseURL: Database{
+			URL: os.Getenv("DATABASE_URL"),
+		},
+		APIKeys: API{
+			TMDBKey: os.Getenv("TMDB_API_KEY"),
+		},
+		BotTokens: Bot{
+			DiscordToken:  os.Getenv("DISCORD_BOT_TOKEN"),
+			TelegramToken: os.Getenv("TELEGRAM_BOT_TOKEN"),
+		},
+		Server: Server{
+			Port: os.Getenv("PORT"),
+			Host: os.Getenv("HOST"),
+		},
+	}, nil
 }
