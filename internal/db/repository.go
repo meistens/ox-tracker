909e3d3c0644194867b3f211407d1ce34a8e43f4
David Mebo <krankd@proton.me>
2025-06-29 16:14:02 +0100

    commands run for the db

---
+++ internal/db/repository.go
@@ -0,0 +1,24 @@
+package db
+
+import "mtracker/internal/models"
+
+// UserRepository handles user-related database ops
+type UserRepository struct {
+	db *DB
+}
+
+func NewUserRepository(db *DB) *UserRepository {
+	return &UserRepository{db: db}
+}
+
+func (r *UserRepository) Create(user *models.User) error {
+	query := `
+	INSERT INTO users (id, username, platform, uodated_at)
+	VALUES ($1, $2, $3, CURRENT_TIMESTAMP)
+	ON CONFLICT (id) DO UPDATE SET
+	username = $2, platform = $3, updated_at = CURRENT_TIMESTAMP
+	RETURNING created_at`
+
+	err := r.db.QueryRow(query, user.ID, user.Username, user.Platform).Scan(&user.CreatedAt)
+	return err
+}
