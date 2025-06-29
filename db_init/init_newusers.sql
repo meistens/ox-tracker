9024d9a3a543fce2495bc026e92a71c5dd47b9d3
David Mebo <krankd@proton.me>
2025-06-29 11:49:46 +0100

    tweaked script to work with current workflow (testrun!)

--- db_init/init_newusers.sql
+++ db_init/init_newusers.sql
@@ -1,24 +1,24 @@
 -- Create a new user and database for your app
-CREATE USER myappuser
-WITH
-    PASSWORD 'myapppassword';
+-- CREATE USER oxbreeze
+-- WITH
+--     PASSWORD 'passwrd';
 
-CREATE DATABASE myappdb OWNER myappuser;
+CREATE DATABASE oxtracker OWNER oxbreeze;
 
 -- (Optional) Connect to the new database and grant privileges
-\connect myappdb
+\connect oxtracker
 
 -- Grant all privileges on the public schema to the app user (default for new DBs)
-GRANT ALL PRIVILEGES ON SCHEMA public TO myappuser;
+GRANT ALL PRIVILEGES ON SCHEMA public TO oxbreeze;
 
 -- Grant all privileges on all tables, sequences, and functions in the public schema
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO myappuser;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO myappuser;
-GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public TO myappuser;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO oxbreeze;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO oxbreeze;
+GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public TO oxbreeze;
 
 -- Ensure future tables, sequences, and functions are accessible
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON TABLES TO myappuser;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON SEQUENCES TO myappuser;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON FUNCTIONS TO myappuser;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON TABLES TO oxbreeze;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON SEQUENCES TO oxbreeze;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON FUNCTIONS TO oxbreeze;
 
 -- You can add more users/databases as needed
