b161596704451d49772d1a81044f91526e34baab
David Mebo <krankd@proton.me>
2025-06-30 16:42:05 +0100

    clean-up test code reused from previous project to test db connection(previous project had postgres with ssl disabled)

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -58,26 +58,3 @@ func main() {
 
 	log.Println("Server shutdown complete")
 }
-
-// import (
-// 	"database/sql"
-// 	"fmt"
-// 	"log"
-
-// 	_ "github.com/lib/pq"
-// )
-
-// func main() {
-// 	fmt.Println("Starting connection with Postgres Db")
-// 	connStr := "postgres://postgres:postgres@localhost:5432/postgres"
-// 	db, err := sql.Open("postgres", connStr)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	defer db.Close()
-// 	if err = db.Ping(); err != nil {
-// 		log.Println("DB Ping Failed")
-// 		log.Fatal(err)
-// 	}
-// 	log.Println("DB Connection started successfully")
-// }
